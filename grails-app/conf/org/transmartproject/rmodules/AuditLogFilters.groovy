ede003d6409057faf7f5fbff8b584417f590e5d3
Gijs Kant <gijs@thehyve.nl>
2016-02-01 17:15:08 +0100

    Implement audit metrics for Rmodules.

--- grails-app/conf/org/transmartproject/rmodules/AuditLogFilters.groovy
+++ grails-app/conf/org/transmartproject/rmodules/AuditLogFilters.groovy
@@ -5,6 +5,7 @@ import org.transmartproject.core.users.User
 class AuditLogFilters {
 
     def accessLogService
+    def auditLogService
     User currentUserBean
 
     def filters = {
@@ -16,6 +17,16 @@ class AuditLogFilters {
                             eventMessage: "User (IP: ${ip}) downloaded ${params.path} for ${params.analysisName}",
                             requestURL: "${request.forwardURI}${request.queryString ? '?' + request.queryString : ''}")
                 }
+            }
+        }
+        jobs(controller: 'asyncJob', action: 'createnewjob') {
+            after = { model ->
+                def workflow = params.jobType
+                def action = params.variablesConceptPaths
+                auditLogService.report("Advanced Workflow - ${workflow}", request,
+                    user: currentUserBean,
+                    action: action as String,
+                )
             }
         }
     }
